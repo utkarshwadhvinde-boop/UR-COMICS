ade71760446199933f8f5e5fb83da772051801b4
utkarshwadhvinde-boop <utkarshwadhvinde@gmail.com>
2026-05-13 13:23:29 +0530

    Update Create.tsx

--- src/frontend/src/pages/Create.tsx
+++ src/frontend/src/pages/Create.tsx
@@ -9,7 +9,6 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import {
   isStoppedCanisterError,
-  useCreateChapter,
   useCreateComic,
   useListChapters,
   usePublishChapter,
