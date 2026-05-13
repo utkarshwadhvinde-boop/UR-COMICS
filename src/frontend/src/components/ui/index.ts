5447ffaf00dc247e04ff4c55de507bf8fc47e8df
utkarshwadhvinde-boop <utkarshwadhvinde@gmail.com>
2026-05-13 12:30:56 +0530

    Create index.ts

---
+++ src/frontend/src/components/ui/index.ts
@@ -0,0 +1,4 @@
+export { ComicCard } from "./ComicCard";
+export { SkeletonCard } from "./SkeletonCard";
+export { SkeletonRow } from "./SkeletonRow";
+export { GenreChip, ALL_GENRES } from "./GenreChip";
